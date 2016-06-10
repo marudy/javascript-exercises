858b1381bae33d4aa7650c48c70f6ec9ab268674
marudy <maroudasa@gmail.com>
2016-06-10 22:15:40 +0300

    Added new version that utilizes array method forEach()

--- string-calculator.js
+++ string-calculator.js
@@ -1,3 +1,7 @@
+/*
+ * Choose from below the version you prefer to use!
+ */
+
 function add(input) {
   var sum = 0;
   
@@ -7,6 +11,22 @@ function add(input) {
    	for (var i=0; i < numbers.length; i++) {
   		sum = sum + parseFloat(numbers[i]);
     }
+    return sum;
+  } else {
+  	return input;
+  }
+}
+
+// New version of add() function that utilizes array methodforEach()
+function add(input) {
+  var sum = 0;
+  
+  if (input.indexOf(',') > -1) {
+  	var numbers = input.split(',');
+   
+   	numbers.forEach(function(currentValue, index, array) {
+   	  sum += parseFloat(array[index])
+   	});
     return sum;
   } else {
   	return input;
