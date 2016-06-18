6ecabe11bb77290288322cf32ae0035eb9ea5e84
maroudasa@gmail.com <maroudasa@gmail.com>
2016-06-18 18:48:26 +0300

    Added missing semicolon

--- string-calculator.js
+++ string-calculator.js
@@ -1,17 +1,17 @@
 /*
  * Choose from below the version you prefer to use!
  *
- * USAGE TIP: If you want to supply as input argument an array, call the function using 
+ * USAGE TIP: If you want to supply as input argument an array, call the function using
  * the apply() method of any function object.
  * Invocation example: add.apply(null, [1, 2]);
  */
 
 function add(input) {
   var sum = 0;
-  
+
   if (input.indexOf(',') > -1) {
   	var numbers = input.split(',');
-   
+
    	for (var i=0; i < numbers.length; i++) {
   		sum = sum + parseFloat(numbers[i]);
     }
@@ -24,12 +24,12 @@ function add(input) {
 // New version of add(input) function that utilizes array methodforEach()
 function add(input) {
   var sum = 0;
-  
+
   if (input.indexOf(',') > -1) {
   	var numbers = input.split(',');
-   
+
    	numbers.forEach(function(currentValue, index, array) {
-   	  sum += parseFloat(array[index])
+   	  sum += parseFloat(array[index]);
    	});
     return sum;
   } else {
